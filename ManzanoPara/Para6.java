82542f8b00b179d4e1502d7834d68e3d3832663c
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-12-16 08:42:03 -0300

    divisíveis por 4 que sejam menores que 200

---
+++ ManzanoPara/Para6.java
@@ -0,0 +1,12 @@
+package manzanoPara;
+
+public class Para6 {
+	public static void main(String[] args) {
+        for(int numero = 1; numero < 199; numero++){
+            if(numero % 4 == 0){
+
+                System.out.println(numero);
+            }
+        }
+	}
+}
