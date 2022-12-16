d7525faf849c62acf72780c7d1bf90c14fc1d1ce
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-12-16 08:32:20 -0300

    quadrados de 15 a 200

---
+++ ManzanoPara/Para1.java
@@ -0,0 +1,15 @@
+package manzanoPara;
+
+public class Para1 {
+
+	public static void main(String[] args) {
+		int quadrado = 0;
+		
+
+        for(int numero = 15; numero < 200; numero++){
+            quadrado = numero*numero;
+            System.out.println(quadrado);
+        }
+	}
+
+}
