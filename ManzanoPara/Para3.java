691c8fef31da3691b8cc21b5bfa97c7c10562af7
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-12-16 08:34:25 -0300

    soma primeiros 100 inteiros

---
+++ ManzanoPara/Para3.java
@@ -0,0 +1,14 @@
+package manzanoPara;
+
+public class Para3 {
+
+	public static void main(String[] args) {
+        int resultado = 0;
+        for(int numero = 1; numero < 101; numero++){
+            resultado = resultado+numero;
+            
+        }
+        System.out.println(resultado);
+	}
+
+}
