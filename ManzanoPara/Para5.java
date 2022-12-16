251cf983f403d756e42947ee278d57f2e1054bad
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-12-16 08:40:30 -0300

    Apresentar os valores ímpares de 0 a 20.

---
+++ ManzanoPara/Para5.java
@@ -0,0 +1,13 @@
+package manzanoPara;
+
+public class Para5 {
+	public static void main(String[] args) {
+
+        for(int numero = 0; numero < 20; numero++){
+            if(numero % 2 != 0){
+
+               System.out.println(numero);
+            }
+        }
+	}
+}
