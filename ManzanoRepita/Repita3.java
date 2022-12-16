6900a94b1fc36b6fa8a61e94781800a4cbf57ba5
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-12-16 09:09:13 -0300

    todos os números divisíveis por 4

---
+++ ManzanoRepita/Repita3.java
@@ -0,0 +1,16 @@
+package manzanoRepita;
+
+public class Repita3 {
+	public static void main(String[] args) {
+		 int contadora= 1;
+         do {
+             if(contadora%4==0){
+                 System.out.println(contadora);
+                 
+             }
+             contadora++;
+
+         } while (contadora < 201);
+
+	}
+}
